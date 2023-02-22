b2d8b902a87415fc862348bc627bf37ff7452856
Mukunthan <smukunthan134@gmail.com>
2023-02-22 14:55:19 +0530

    After axios

--- src/Home.js
+++ src/Home.js
@@ -12,6 +12,7 @@ const Home = ({posts}) => {
           </p>
         )
       }
+      
     </main>
   )
 }
--- src/Post.js
+++ src/Post.js
@@ -14,6 +14,7 @@ const Post = ({post}) => {
                   : `${(post.body).slice(0,25)}...`
             }
         </p>
+       
     </article>
   )
 }
